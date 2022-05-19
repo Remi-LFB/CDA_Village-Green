c33052c11a1e3ec974ade04f4b0551697a5b74c9
Rémi LFB <remi.lfb@gmail.com>
2022-05-19 16:55:01 +0200

    Mise à jour du MCD et des Fixtures (en cours)

--- Symfony/src/DataFixtures/SupplierFixtures.php
+++ Symfony/src/DataFixtures/SupplierFixtures.php
@@ -11,6 +11,7 @@ class SupplierFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $supplier1 = new Supplier();
+        $supplier2 = new Supplier();
 
         $supplier1->setName('Ibanez');
         $supplier1->setType('Constructeur');
@@ -22,5 +23,16 @@ class SupplierFixtures extends Fixture
         $supplier1->setCity('Chiyoda City');
         $supplier1->setCountry('Japon');
         $manager->persist($supplier1);
+
+        $supplier2->setName('Guitars & Tools');
+        $supplier2->setType('Importateur');
+        $supplier2->setContact('Robert Lafarce');
+        $supplier2->setMail('robert.lafarce@gnt.fr');
+        $supplier2->setPhone('0203040506');
+        $supplier2->setAddress('42b Avenue de la Sainte-Guitare');
+        $supplier2->setZipcode('80000');
+        $supplier2->setCity('Amiens');
+        $supplier2->setCountry('France');
+        $manager->persist($supplier2);
     }
 }
