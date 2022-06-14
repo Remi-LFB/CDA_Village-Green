bb73df663bafe9a7965d14924e39d2e88e67631f
Rémi LFB <remi.lfb@gmail.com>
2022-06-14 23:30:26 +0200

    Update

--- Symfony/src/Controller/CategoryController.php
+++ Symfony/src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\SubCategory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     #[Route('/category/{category}', name: 'category_show')]
-    public function index(Category $category): Response
+    public function categoryShow(Category $category): Response
     {
-        return $this->render('category/index.html.twig', [
+        return $this->render('category/category.html.twig', [
             'categoryName' => $category->getName(),
             'subCategories' => $category->getSubCategories()
+        ]);
+    }
+
+    #[Route('/subcategory/{subCategory}', name: 'subcategory_show')]
+    public function subCategoryShow(SubCategory $subCategory): Response
+    {
+        return $this->render('category/subcategory.html.twig', [
+            'categoryName' => $subCategory->getCategory()->getName(),
+            'subCategoryName' => $subCategory->getName(),
+            'products' => $subCategory->getProducts()
         ]);
     }
 }
